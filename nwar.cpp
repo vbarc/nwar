f7e173a3807d27fd98969790c658fb2b81ea5ad3
vbarc <154655335+vbarc@users.noreply.github.com>
2023-12-24 12:04:19 -0800

    Minor edits

--- nwar.cpp
+++ nwar.cpp
@@ -29,7 +29,6 @@ static const float vertices[] = {
 };
 
 int main(void) {
-    
     glfwSetErrorCallback([](int error, const char* description) { fprintf(stderr, "Error: %s\n", description); });
 
     if (!glfwInit()) {
