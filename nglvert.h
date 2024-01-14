ebd9b35aea8a1c28beb5dc6a8f135934ecef68ef
vbarc <154655335+vbarc@users.noreply.github.com>
2024-01-13 20:15:52 -0800

    Adjust light position a little

--- nglvert.h
+++ nglvert.h
@@ -17,7 +17,7 @@ layout (std140, binding = 0) uniform FrameUniform {
     int is_wireframe_enabled;
 };
 
-const vec3 light_position = vec3(-1100, 1000, 1000);
+const vec3 light_position = vec3(-1100, 1200, 1000);
 const vec3 ambient = vec3(0.1, 0.1, 0.1);
 const vec3 diffuse_albedo = vec3(0, 0.7, 0);
 const vec3 specular_albedo = vec3(0.1);
