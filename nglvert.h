9d10d70680dd6dbe7dc73f2f94c0eae802e0437a
vbarc <154655335+vbarc@users.noreply.github.com>
2024-01-15 12:45:21 -0800

    Distrubute instances based on gl_InstanceID

--- nglvert.h
+++ nglvert.h
@@ -19,6 +19,19 @@ layout (std140, binding = 0) uniform FrameUniform {
     int is_wireframe_enabled;
 };
 
+const ivec2 unit_size = ivec2(10, 10);
+const ivec2 regiment_size = ivec2(3, 5);
+const int unit_instance_count = unit_size.x * unit_size.y;
+const int regiment_unit_count = regiment_size.x * regiment_size.y;
+const int regiment_instance_count = regiment_unit_count * unit_instance_count;
+
+const vec2 in_unit_distance = vec2(4, 4);
+const vec2 unit_padding = vec2(10, 10);
+const vec2 unit_distance = unit_size * in_unit_distance + unit_padding;;
+const float regiment_padding = 20;
+const float regiment_distance = regiment_size.y * unit_distance.y + regiment_padding;
+const vec2 regiment_psize = regiment_size * unit_distance - unit_padding;
+
 const vec3 light_position = vec3(-1100, 1200, 1000);
 const vec3 specular_k = vec3(0.1);
 const float specular_power = 48;
@@ -28,7 +41,26 @@ void main() {
     if (gl_InstanceID == 0) {
         position = in_position;
     } else {
-        position = in_position + vec3(4 * gl_InstanceID, 0, 0);
+        int index = gl_InstanceID;
+
+        int regiment_index = index / regiment_instance_count;
+        index = index % regiment_instance_count;
+
+        int unit_index = index / unit_instance_count;
+        index = index % unit_instance_count;
+
+        int unit_i = unit_index % regiment_size.x;
+        int unit_j = unit_index / regiment_size.x;
+
+        int in_unit_i = index % unit_size.x;
+        int in_unit_j = index / unit_size.x;
+
+        float x = unit_i * unit_distance.x + in_unit_i * in_unit_distance.x;
+        float y = unit_j * unit_distance.y + in_unit_j * in_unit_distance.y + regiment_index * regiment_distance;
+        x = x - regiment_psize.x / 2;
+        y = y * -1 + regiment_distance;
+
+        position = in_position + vec3(x, 0, y);
     }
 
     vec3 light_vector = normalize(light_position - position);
