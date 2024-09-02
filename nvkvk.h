27ad7cc8186dcd37a67b72f51c4212e7e0233a96
vbarc <154655335+vbarc@users.noreply.github.com>
2024-09-02 08:19:42 -0700

    Add nwglm.h

--- nvkvk.h
+++ nvkvk.h
@@ -6,6 +6,4 @@
 #define GLFW_EXPOSE_NATIVE_WIN32
 #include <GLFW/glfw3native.h>
 
-#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
-#include <glm/glm.hpp>
-#include <glm/ext.hpp>
+#include "nwglm.h"
---
+++ nwglm.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
