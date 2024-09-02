cc77a0a2ba754becfb2ac2a1da20d5f7c0cf41b6
vbarc <154655335+vbarc@users.noreply.github.com>
2024-09-02 08:13:01 -0700

    Unify glm usage

--- nvkvk.h
+++ nvkvk.h
@@ -6,10 +6,6 @@
 #define GLFW_EXPOSE_NATIVE_WIN32
 #include <GLFW/glfw3native.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#define GLM_ENABLE_EXPERIMENTAL
-#include <glm/gtx/hash.hpp>
+#include <glm/ext.hpp>
