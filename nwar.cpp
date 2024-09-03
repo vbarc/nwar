389271df1c87076fc61142290155d6940b4657b3
vbarc <154655335+vbarc@users.noreply.github.com>
2024-09-02 21:07:37 -0700

    Update TODOs

--- nwar.cpp
+++ nwar.cpp
@@ -3,7 +3,19 @@
 
 int main() {
     // TODO: Convert to Vulkan
-    // TODO: Gamma correction
+    //   More drammatic colors
+    //   Sky
+    //   Army
+    //   Layers?
+    //   Sound
+    // TODO: VR
+    //   Build helloxr
+    //   Run helloxr on Quest
+    //   Make NwVk look more like helloxr
+    //   Replace helloxr with NwVk
+    //   Run NwVk on Quest
+    //   Run on X
+    // === MVP cut-off line ===
     // TODO: Shadows
     // TODO: Nicer grass rendering, texture
     // TODO: Nicer cloth rendering, texture, roughness, cloth look
